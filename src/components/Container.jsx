9526e72ca04cf612977b38623469ec49deaf44b3
Melanie Rodas <mrodasl@uni.pe>
2022-01-06 16:36:40 -0500

    add items into Container component

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,17 +1,34 @@
 import React from "react";
+import { useState } from "react";
+
 import { Home } from "./Home";
 import FormTodo from "./FormTodo";
 import TaskList from "./TaskList";
 
-const container = () => {
+const Container = () => {
+  const [list, setList] = useState([]); // (B-1)
+
+  // (A-2)
+  const handleAddItem = (addItem) => {
+    setList([...list, addItem]); // (B-2)
+  };
   return (
     <div>
       Container!
       <Home />
-      <FormTodo />
-      <TaskList />
+      {/*(A-1)*/}
+      <FormTodo handleAddItem={handleAddItem} />
+      {/*(C)*/}
+      <TaskList list={list} setList={setList} />
     </div>
   );
 };
 
-export default container;
+export default Container;
+
+/* 
+B-1) We are going to create the list state, which will be used to store the data.
+C)We add the list and setList attributes in the TaskList component, and pass the state to it.
+
+
+*/

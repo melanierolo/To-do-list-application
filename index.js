6f0c71846aec16b689947c2c53b0f1b601ff9552
Melanie Rodas <mrodasl@uni.pe>
2022-06-16 13:09:56 -0500

    refactored form.js

--- index.js
+++ index.js
@@ -17,24 +17,11 @@ function App() {
     },
   ]);
 
-  const [value, setValue] = React.useState("");
-
-  /*A-2) I wrote  the handleSubmit function(arrow funciton-ES6)*/
-  const handleSubmit = (e) => {
-    /*By default, the behavior is to reload the page, and we don't want that to take place. 
-    So, we will simply enter, prevent that by entering 'preventDefault();'.*/
-    e.preventDefault();
-
-    /*Next, we will check the value of the field. And if the '(!value)' is empty, then we will 'return;', meaning do nothing. */
-    if (!value) return;
-    const newTodos = [...todos, { text: value, isCompleted: false }];
-    {
-      /* we're going from the old state or the current state to the new state*/
-    }
+  const addTodo = (text) => {
+    const newTodos = [...todos, { text: text, isCompleted: false }];
+    console.log(newTodos);
     setTodos(newTodos);
-    setValue("");
   };
-
   const removeTodo = (e) => {
     console.log(e.target.getAttribute("id"));
     var index = Number(
@@ -49,20 +36,7 @@ function App() {
 
   return (
     <>
-      {/* A-1) We write our form:*/}
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          className="input"
-          value={
-            value /* the value that we just defined within the state of the application*/
-          }
-          placeholder="Add Todo..." /*to give the user some indication as what to do in this field  */
-          onChange={
-            (e) => setValue(e.target.value) /*It will give us the input */
-          }
-        />
-      </form>
+      <TodoForm addTodo={addTodo} />
       <section>
         {todos.map((todo, i) => (
           <div key={i} id={i}>
